da258983316483fb42766047a7a136fd94a70fff
WilliAn-333 <www.pegazo23@gmail.com>
2024-11-29 16:14:05 -0500

    Creacion del proyecto automóvil

---
+++ src/main/java/ec/edu/espoch/automovil3/Automovil3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.automovil3;
+
+/**
+ *
+ * @author USER
+ */
+public class Automovil3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
