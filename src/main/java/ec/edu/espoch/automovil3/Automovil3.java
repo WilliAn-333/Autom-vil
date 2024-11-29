43177ea0648965eb8e4525f702aee0fbe7097e44
WilliAn-333 <www.pegazo23@gmail.com>
2024-11-29 16:58:28 -0500

    cambios inesperados

--- src/main/java/ec/edu/espoch/automovil3/Automovil3.java
+++ src/main/java/ec/edu/espoch/automovil3/Automovil3.java
@@ -11,12 +11,8 @@ package ec.edu.espoch.automovil3;
 public class Automovil3 {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("AUTOMOVIL");
-=======
         System.out.println("Hello World!");
         
         System.out.println("1258255");
->>>>>>> dev-Willian
     }
 }
