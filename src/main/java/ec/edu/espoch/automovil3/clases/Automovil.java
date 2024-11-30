29554f3da1cdefb2edc209475bc32ca9cc7cddb0
OswaldoBifarini <jgbifarini@gmail.com>
2024-11-29 23:54:54 -0500

    enumeracion combustible

--- src/main/java/ec/edu/espoch/automovil3/clases/Automovil.java
+++ src/main/java/ec/edu/espoch/automovil3/clases/Automovil.java
@@ -2,5 +2,180 @@ package ec.edu.espoch.automovil3.clases;
 
 //clase autommovil
 public class Automovil {
-    
+
+    /*creacion de las caracteristicas de la clase
+    -----ATRIBUTOS-------*/
+    private String marca;
+    private int modelo;
+    private double motor;
+    private TipoCombustible tipoCombustible;
+    private TipoAutomovil tipoAutomovil;
+    private int numeroPuertas;
+    private int cantidadAsientos;
+    private double velocidadMaxima;
+    private Color color;
+    private double velocidadActual;
+
+    /*creacion del constructor Automovil
+    -----CONSTRUCTOR-------*/
+    public Automovil(String marca, int modelo, double motor, TipoCombustible tipoCombustible,
+            TipoAutomovil tipoAutomovil, int numeroPuertas, int cantidadAsientos,
+            double velocidadMaxima, Color color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.motor = motor;
+        this.tipoCombustible = tipoCombustible;
+        this.tipoAutomovil = tipoAutomovil;
+        this.numeroPuertas = numeroPuertas;
+        this.cantidadAsientos = cantidadAsientos;
+        this.velocidadMaxima = velocidadMaxima;
+        this.color = color;
+        this.velocidadActual = 0;
+    }
+
+    // Métodos Getters y Setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getMotor() {
+        return motor;
+    }
+
+    public void setMotor(double motor) {
+        this.motor = motor;
+    }
+
+    public TipoCombustible getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(TipoCombustible tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public TipoAutomovil getTipoAutomovil() {
+        return tipoAutomovil;
+    }
+
+    public void setTipoAutomovil(TipoAutomovil tipoAutomovil) {
+        this.tipoAutomovil = tipoAutomovil;
+    }
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public void setNumeroPuertas(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    public int getCantidadAsientos() {
+        return cantidadAsientos;
+    }
+
+    public void setCantidadAsientos(int cantidadAsientos) {
+        this.cantidadAsientos = cantidadAsientos;
+    }
+
+    public double getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public void setVelocidadMaxima(double velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public double getVelocidadActual() {
+        return velocidadActual;
+    }
+
+    public void setVelocidadActual(double velocidadActual) {
+        this.velocidadActual = velocidadActual;
+    }
+
+    // Métodos funcionales
+    public void acelerar(double incremento) {
+        if (velocidadActual + incremento > velocidadMaxima) {
+            System.out.println("No puedes superar la velocidad máxima: " + velocidadMaxima + " km/h.");
+        } else {
+            velocidadActual += incremento;
+            System.out.println("Velocidad actual: " + velocidadActual + " km/h.");
+        }
+    }
+
+    public void desacelerar(double decremento) {
+        if (velocidadActual - decremento < 0) {
+            System.out.println("No puedes desacelerar a una velocidad negativa.");
+        } else {
+            velocidadActual -= decremento;
+            System.out.println("Velocidad actual: " + velocidadActual + " km/h.");
+        }
+    }
+
+    public void frenar() {
+        velocidadActual = 0;
+        System.out.println("El automóvil se ha detenido.");
+    }
+
+    public double calcularTiempoLlegada(double distancia) {
+        if (velocidadActual == 0) {
+            System.out.println("El automóvil está detenido. No se puede calcular el tiempo de llegada.");
+            return -1;
+        }
+        double tiempo = distancia / velocidadActual;
+        System.out.println("Tiempo estimado de llegada: " + tiempo + " horas.");
+        return tiempo;
+    }
+
+    public void mostrarAtributos() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Motor: " + motor + "L");
+        System.out.println("Tipo de Combustible: " + tipoCombustible);
+        System.out.println("Tipo de Automóvil: " + tipoAutomovil);
+        System.out.println("Número de Puertas: " + numeroPuertas);
+        System.out.println("Cantidad de Asientos: " + cantidadAsientos);
+        System.out.println("Velocidad Máxima: " + velocidadMaxima + " km/h");
+        System.out.println("Color: " + color);
+        System.out.println("Velocidad Actual: " + velocidadActual + " km/h");
+    }
+
+    // Método Main
+    public static void main(String[] args) {
+        // Crear un objeto Automovil
+        Automovil auto = new Automovil("Toyota", 2022, 1.8, TipoCombustible.GASOLINA,
+                TipoAutomovil.CIUDAD, 4, 5, 180, Color.ROJO);
+
+        // Operaciones
+        auto.mostrarAtributos();
+        auto.setVelocidadActual(100);
+        System.out.println("Velocidad inicial: " + auto.getVelocidadActual() + " km/h.");
+
+        auto.acelerar(20);
+        auto.desacelerar(50);
+        auto.frenar();
+
+        auto.calcularTiempoLlegada(200);
+    }
 }
---
+++ src/main/java/ec/edu/espoch/automovil3/clases/tipoCombustible/TipoCombustible.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.automovil3.clases.tipoCombustible;
+
+/**
+ *
+ * @author DELL
+ */
+public class TipoCombustible {
+   
+    GASOLINA,
+    BIOETANOL,
+    DIESEL,
+    BIODIESEL,
+    GAS_NATURAL
+}
+
+    
+
